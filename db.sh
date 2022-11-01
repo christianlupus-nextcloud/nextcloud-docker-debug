60797aa4dc9ca65d7d608607c190afe0e8ef7bf3
Christian Wolf <github@christianwolf.email>
2022-11-01 08:53:02 +0100

    Replace DB extraction script with more general one capable of scripting

--- db.sh
+++ db.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker-compose exec db mysql -u nextcloud -pnextcloud_pwd nextcloud
+if [ -t 0 ]; then
+	docker-compose exec db mysql -u nextcloud -pnextcloud_pwd nextcloud
+else
+	echo 'Running without tty'
+	docker-compose exec -T db mysql -u nextcloud -pnextcloud_pwd nextcloud
+fi
+
 
